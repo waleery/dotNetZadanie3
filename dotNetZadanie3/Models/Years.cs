ce76d66ec32956a4b5e0c17eb736fd9a165a6407
waleery <walenda1997@gmail.com>
2022-03-23 16:27:16 +0100

    add logigic checking leap year

---
+++ dotNetZadanie3/Models/Years.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace dotNetZadanie3.Models
+{
+    public class Years
+    {
+        [Display(Name = "Imie")]
+        [Required(ErrorMessage = "Pole '{0}' jest wymagane!"), StringLength(10, ErrorMessage = "Pole {0} musi być krótsze niż {2} liter!")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Rok urodzenia")]
+        [Required(ErrorMessage = "Pole '{0}' jest wymagane!"), Range(1899, 2022, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}")]
+        public int? Year { get; set; }
+
+        public string YearCheck(int? number)
+        {
+            if(number % 4 == 0)
+            {
+                if(number % 100 == 0)
+                {
+                    if(number % 400 == 0)
+                    {
+                        return "To był rok przestepny.";
+                    }
+                    else
+                    {
+                        return "To nie był rok przestepny.";
+                    }
+                }
+                else
+                {
+                    return "To był rok przestepny.";
+                }
+            }
+            else
+            {
+                return "To nie był rok przestepny.";
+            }
+        }
+    }
+}
+
---
+++ dotNetZadanie3/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using dotNetZadanie3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotNetZadanie3.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    [BindProperty]
+    public Years Year {get; set;}
+
+    
+
+    public string Text { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        else
+        {
+            ViewData["Result"] = Year.Name + " urodził się  w "+ Year.Year +". " + Year.YearCheck(Year.Year);
+            return Page();
+        }
+    }
+}
+
