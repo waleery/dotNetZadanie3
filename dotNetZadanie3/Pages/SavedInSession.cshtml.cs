a0b4806220b924808dc89c82248ad1059fe27a03
waleery <walenda1997@gmail.com>
2022-03-24 14:14:54 +0100

    add SavedInSession page

---
+++ dotNetZadanie3/Pages/SavedInSession.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotNetZadanie3.Pages
+{
+    public class SavedInSessionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
