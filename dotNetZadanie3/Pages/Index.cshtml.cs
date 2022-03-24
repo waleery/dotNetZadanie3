99140207e09143b7b3e8f1aa7a21cc69a8ca675b
waleery <walenda1997@gmail.com>
2022-03-24 14:44:32 +0100

    added: saving and read in session; displaying data in savedInSession

--- dotNetZadanie3/Pages/Index.cshtml.cs
+++ dotNetZadanie3/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using dotNetZadanie3.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace dotNetZadanie3.Pages;
 
@@ -13,7 +15,7 @@ public class IndexModel : PageModel
 
     
 
-    public string Text { get; set; }
+    //public string Text { get; set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -33,6 +35,9 @@ public class IndexModel : PageModel
         }
         else
         {
+            HttpContext.Session.SetString("Data", JsonConvert.SerializeObject(Year));
+            //HttpContext.Session.SetString("Data", JsonConvert.SerializeObject(Text));
+            //return RedirectToPage("./SavedInSession");
             ViewData["Result"] = Year.Name + Year.NameCheck(Year.Name) + " się  w " + Year.Year +". " + Year.YearCheck(Year.Year);
             return Page();
         }
--- dotNetZadanie3/Pages/SavedInSession.cshtml.cs
+++ dotNetZadanie3/Pages/SavedInSession.cshtml.cs
@@ -1,12 +1,24 @@
+using dotNetZadanie3.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+
 
 namespace dotNetZadanie3.Pages
 {
     public class SavedInSessionModel : PageModel
     {
+        public Years Year { get; set; }
         public void OnGet()
         {
+            var Data = HttpContext.Session.GetString("Data");
+
+            if (Data != null)
+            {
+                Year = JsonConvert.DeserializeObject<Years>(Data);
+            }
         }
     }
 }
