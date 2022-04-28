1d93551c54b25a81f7fbd0749d1143da7cc717df
waleery <walenda1997@gmail.com>
2022-04-28 21:37:05 +0200

    Stworzenie DTO

---
+++ dotNetZadanie3/ViewModels/Years/ListYearsForListVM.cs
@@ -0,0 +1,9 @@
+﻿namespace dotNetZadanie3.ViewModels.Years
+{
+	public class ListYearsForListVM
+	{
+		public List<YearsForListVM> Years { get; set; }
+		public int Count { get; set; }
+
+	}
+}
---
+++ dotNetZadanie3/ViewModels/Years/YearsForListVM.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotNetZadanie3.ViewModels.Years
+{
+	public class YearsForListVM
+	{
+		public int? Id { get; set; }
+
+		[Display(Name = "Imie i nazwisko")]
+		[Required(ErrorMessage = "Pole '{0}' jest wymagane!"),
+		StringLength(100, ErrorMessage = "Pole {0} musi być krótsze niż {2} liter!")]
+		public string FullName { get; set; }
+
+		[Display(Name = "Rok urodzenia")]
+		[Required(ErrorMessage = "Pole '{0}' jest wymagane!"), 
+		Range(1899, 2022, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}")]
+		public int? Year { get; set; }
+
+
+		[Display(Name = "Wynik zapytania")]
+		public String? wynik { get; set; }
+	}
+}
